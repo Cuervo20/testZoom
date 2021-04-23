2b57c308401b78428eb25ebd35472b8898a1e9a8
Skin206080 <skindlns@gmail.com>
2021-04-23 18:08:11 -0500

    change

---
+++ pages/Zoom.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <zoom-window/> 
+</div>
+
+
+</template>
+
+
+<script>
+import ZoomWindow from '../components/ZoomWindow'
+
+export default {
+  components: {
+    ZoomWindow
+  },
+
+}
+
+</script>
